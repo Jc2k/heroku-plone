0cb5d7ea8d7834aef70b5958faee7411703e764a
John Carr <john.carr@isotoma.com>
2011-09-20 20:01:09 +0100

    Rewrite zope instance to serve on requested port

--- awesome/app.py
+++ awesome/app.py
@@ -1,15 +1,51 @@
-from Zope2.Startup.run import configure
-from Zope2 import startup
-import os
+import os, sys
+
+zope_conf = """
+instancehome %(instance_home)s
+
+<http-server>
+  address %(port)s
+</http-server>
+
+<zodb_db main>
+#    <filestorage>
+#      path %(instance_home)s/var/Data.fs
+#    </filestorage>
+    <temporarystorage>
+      name temporary storage for main data
+    </temporarystorage>
+    mount-point /
+    container-class Products.TemporaryFolder.TemporaryContainer
+</zodb_db>
+
+<zodb_db temporary>
+    <temporarystorage>
+      name temporary storage for sessioning
+    </temporarystorage>
+    mount-point /temp_folder
+    container-class Products.TemporaryFolder.TemporaryContainer
+</zodb_db>
+
+"""
 
 root_dir = os.path.join(os.path.dirname(__file__), "..")
-conf_dir = os.path.join(root_dir, "zope", "etc", "zope.conf")
+instance_home = os.path.join(root_dir, "zope")
+conf_file = os.path.join(instance_home, "etc", "zope.conf")
+zdctl = os.path.join(root_dir, "bin", "zopectl")
 
-if not os.path.exists(conf_dir):
-    os.system("%(root_dir)s/bin/mkzopeinstance -u zopeadmin:zopeadmin -d %(root_dir)s/zope" % dict(root_dir=root_dir))
+if not os.path.exists(conf_file):
+    os.system("%(root_dir)s/bin/mkzopeinstance -u zopeadmin:zopeadmin -d %(instance_home)s" % dict(root_dir=root_dir, instance_home=instance_home))
 
-configure(conf_dir)
-startup()
 
-# mod_wsgi looks for the special name 'application'.
-from ZPublisher.WSGIPublisher import publish_module as application
+# Rewrite zope.conf according to the port we are supposd to listen on
+open(conf_file, "w").write(zope_conf % dict(
+    instance_home=instance_home,
+    port="9000",
+    ))
+
+
+os.environ["INSTANCE_HOME"] = instance_home
+os.environ["PYTHON"] = sys.executable
+
+print zdctl
+os.execvp(zdctl, [zdctl, "-C", conf_file, "fg"])
